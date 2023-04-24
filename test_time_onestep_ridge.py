8e69f05097d2f732444611d82822731116ee5749
Samuel Vaiter <samuel.vaiter@gmail.com>
2023-04-24 14:50:22 +0200

    add id

--- test_time_onestep_ridge.py
+++ test_time_onestep_ridge.py
@@ -15,16 +15,16 @@ n, p = X.shape
 
 def diff_gd(w0, theta, lr, start_trace=0, max_iter=100):
     t0 = time.time()
-    times = np.zeros(1+max_iter // 100)
+    times = np.zeros(1+max_iter // 1000)
     w = w0
-    ws = np.zeros((1+max_iter // 100, p))
+    ws = np.zeros((1+max_iter // 1000, p))
     dtheta = np.zeros(p)
-    dthetas = np.zeros((1+max_iter // 100, p))
+    dthetas = np.zeros((1+max_iter // 1000, p))
     for i in range(max_iter):
-        if i % 100 == 0:
-            dthetas[i//100, :] = dtheta
-            ws[i//100, :] = w
-            times[i//100] = time.time() - t0
+        if i % 1000 == 0:
+            dthetas[i//1000, :] = dtheta
+            ws[i//1000, :] = w
+            times[i//1000] = time.time() - t0
         dw = XTX @ w - XTy + theta * w
         w = w - lr * dw
         if i >= start_trace:
@@ -36,9 +36,9 @@ def diff_gd(w0, theta, lr, start_trace=0, max_iter=100):
 str_traces = [0, 4000, 5000]
 theta_facs = [0.01, 0.001]
 max_iter = 5001
-norm_dthetahats = np.ones((len(theta_facs), len(str_traces), 2, 1+max_iter // 100))
-norm_ws = np.ones((len(theta_facs), len(str_traces), 2, 1+max_iter // 100))
-times = np.zeros((len(theta_facs), len(str_traces), 2, 1+max_iter // 100))
+norm_dthetahats = np.ones((len(theta_facs), len(str_traces), 2, 1+max_iter // 1000))
+norm_ws = np.ones((len(theta_facs), len(str_traces), 2, 1+max_iter // 1000))
+times = np.zeros((len(theta_facs), len(str_traces), 2, 1+max_iter // 1000))
 for t, theta_fac in enumerate(theta_facs):
     theta = (np.linalg.norm(X,2) ** 2) * theta_fac
     eigs = np.sort(np.linalg.svd(XTX + theta * np.eye(p))[1])[::-1]
@@ -53,16 +53,23 @@ for t, theta_fac in enumerate(theta_facs):
     wsklearn = np.linalg.solve(X.T @ X + theta * np.eye(p), X.T @ y)
     jac_gt = np.linalg.solve(XTX + theta * np.eye(p), -wsklearn)
 
-    t0 = time.time()
-    _ = diff_gd(np.zeros(p), theta, lr, start_trace = max_iter, max_iter = max_iter)
-    t1 = time.time()
+    # t0 = time.time()
+    # _ = diff_gd(np.zeros(p), theta, lr, start_trace = max_iter, max_iter = max_iter)
+    # t1 = time.time()
 
     for j, str_trace in enumerate(str_traces):
         for r, lr in enumerate(lrs):
             what, ws, dthetahats, ltimes = diff_gd(np.zeros(p), theta, lr, start_trace=str_trace, max_iter=max_iter)
             norm_ws[t, j, r, :] = np.linalg.norm(ws - wsklearn, axis=1) / np.linalg.norm(wsklearn)
+            if str_trace == max_iter-1:
+                tid0 = time.time()
+                did = np.linalg.solve(XTX + theta * np.eye(p), -what)
+                dthetahats[-1,:] = did
+                tid = time.time() - tid0
+            else:
+                tid = 0
             norm_dthetahats[t, j, r, :] = np.linalg.norm(dthetahats - jac_gt, axis=1) / np.linalg.norm(jac_gt)
-            times[t, j, r, :] = ltimes
+            times[t, j, r, :] = ltimes + tid
 
 #### DISPLAY
 
@@ -73,16 +80,16 @@ colors = viridis(np.linspace(0, 1, len(str_traces)))
 for t, theta_fac in enumerate(theta_facs):
     for r, lr in enumerate(lrs):
         for j, str_trace in enumerate(str_traces):
-            ax[t, r].scatter(times[t,j,r,::10].T, norm_dthetahats[t,j,r,::10].T, color=colors[j])
-            ax[t, r].plot(times[t,j,r,::10].T, norm_dthetahats[t,j,r,::10].T, '', label='_nolegend_', color=colors[j])
-            ax[t, r].scatter(times[t,j,r,::10].T, norm_ws[t,j,r,::10].T, marker='x', label='_nolegend_', color=colors[j])
-            ax[t, r].plot(times[t,j,r,::10].T, norm_ws[t,j,r,::10].T, '--', label='_nolegend_', color=colors[j])
+            ax[t, r].scatter(times[t,j,r,:].T, norm_dthetahats[t,j,r,:].T, color=colors[j])
+            ax[t, r].plot(times[t,j,r,:].T, norm_dthetahats[t,j,r,:].T, '', label='_nolegend_', color=colors[j])
+            ax[t, r].scatter(times[t,j,r,:].T, norm_ws[t,j,r,:].T, marker='x', label='_nolegend_', color=colors[j])
+            ax[t, r].plot(times[t,j,r,:].T, norm_ws[t,j,r,:].T, '--', label='_nolegend_', color=colors[j])
         ax[t, r].set_yscale("log")
         ax[t, r].set_xlabel("Time (s)")
         ax[t, r].set_title(lrs_s[r])
         ax[t, 0].set_ylabel("Relative Jacobian suboptimality")
 
-legends = ["autodiff", "1-step", "no diff"]
+legends = ["autodiff", "1-step", "implicit diff"]
 fig.legend(legends, loc='upper right', bbox_to_anchor=(1,1), ncol=len(legends), bbox_transform=fig.transFigure)
   
 plt.show()
